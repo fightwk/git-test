83e413f86439d9400f97cb5733d66a2fd6e390be
fightwk <1826954318@qq.com>
2022-09-09 00:00:43 +0800

    second commit

--- src/main/java/com/fightwk/git/GitTest.java
+++ src/main/java/com/fightwk/git/GitTest.java
@@ -11,5 +11,6 @@ package com.fightwk.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello world2!");
     }
 }
