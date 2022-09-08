9709cd0c3515d4d38aafc12e17edfe8418a83b40
fightwk <1826954318@qq.com>
2022-09-08 23:59:04 +0800

    first commit

---
+++ src/main/java/com/fightwk/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.fightwk.git;
+
+/**
+ * @Author fightwk
+ * @Date 2022年09月08日 23:56
+ * @ProjectName git-test
+ * @Package com.fightwk.git
+ * @Version V1.0
+ * @Description
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
