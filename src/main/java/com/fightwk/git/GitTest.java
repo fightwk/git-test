a9c567fee4147c169ba6c17c275fa1cae44fa811
fightwk <1826954318@qq.com>
2022-09-09 00:45:33 +0800

    push commit

--- src/main/java/com/fightwk/git/GitTest.java
+++ src/main/java/com/fightwk/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("Hello world!");
         System.out.println("Hello world2!");
         System.out.println("Hello world3!");
+        System.out.println("push test!");
     }
 }
